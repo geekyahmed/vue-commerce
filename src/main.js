fecfcde3ec6855e5f9eec8becec14bb6c09f70c2
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-09-04 03:08:54 +0100

    add vuesax library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuesax from 'vuesax'
 import App from './App'
 import router from './router'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+
+Vue.use(Vuesax)
 
 Vue.config.productionTip = false
 
